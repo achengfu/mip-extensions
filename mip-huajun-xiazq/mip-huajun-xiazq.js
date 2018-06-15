c1dca7d217cc24f1b71fadbf001b21350c661834
chengfu <873920193@qq.com>
2018-06-15 18:46:09 +0800

    up

--- mip-huajun-xiazq/mip-huajun-xiazq.js
+++ mip-huajun-xiazq/mip-huajun-xiazq.js
@@ -9,37 +9,37 @@ define(function (require) {
     var $ = require('zepto');
     var customElem = require('customElement').create();
 
-    function init(isid) {
+    function init(isid, element) {
         var ba = document.referrer.indexOf('baidu') > -1;
         var bb = (isid === '1219170');
         var bb1 = (isid === '637958');
         var bc = document.referrer.indexOf('www.onlinedown.net') > -1;
         if (ba || bb || bb1 || bc) {
-            $('em.icon').addClass('checkhover');
-            $('#spdownload').hide();
-            $('#download').show();
-            $('.text2').hide();
-            $('.text1').show();
+            $(element).find('em.icon').addClass('checkhover');
+            $(element).find('#spdownload').hide();
+            $(element).find('#download').show();
+            $(element).find('.text2').hide();
+            $(element).find('.text1').show();
         }
         else {
-            $('em.icon').removeClass('checkhover');
-            if ($('#spdownload').attr('href')) {
-                $('#spdownload').show();
-                $('#download').hide();
+            $(element).find('em.icon').removeClass('checkhover');
+            if ($(element).find('#spdownload').attr('href')) {
+                $(element).find('#spdownload').show();
+                $(element).find('#download').hide();
             }
             else {
-                $('#spdownload').hide();
-                $('#download').show();
+                $(element).find('#spdownload').hide();
+                $(element).find('#download').show();
             }
-            $('.text1').hide();
-            $('.text2').show();
+            $(element).find('.text1').hide();
+            $(element).find('.text2').show();
         }
     }
     //
     customElem.prototype.firstInviewCallback = function () {
         var element = this.element;
         var hsId = $(element).attr('hsId');
-        init(hsId);
+        init(hsId, element);
     };
     return customElem;
 });
